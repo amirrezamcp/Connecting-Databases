7760dcd85065b97f97f5045261708e63a1d027ff
amirrezamcp <yt.bigpotato@gmail.com>
2024-03-02 20:26:44 +0330

    select query

---
+++ Database-constans/update.php
@@ -0,0 +1,11 @@
+<?php
+require_once "connection.php";
+class Update extends Database {
+    public function userUpdate( $column, $value, $id, $table) {
+        $sql = "UPDATE $table SET $column = :value WHERE id = :id;";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([':id' => $id, ':value' => $value]);
+        $result = $stmt->rowCount();
+        return $result;
+    }
+}
